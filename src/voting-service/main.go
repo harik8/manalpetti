e64371f0375c6c3765461283f47018c1f2a4f2e6
harik8 <khprasaad@gmail.com>
2026-03-03 22:10:52 +0100

    D-030326: Implement voting-service

--- src/voting-service/main.go
+++ src/voting-service/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"context"
-	"github.com/jackc/pgx/v5"
 	"log"
+	"net/http"
 	"os"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
@@ -15,13 +18,21 @@ func main() {
 	}
 	defer conn.Close(context.Background())
 
-	for {
-		err = conn.Ping(context.Background())
-		if err != nil {
-			log.Printf("ping failed: %v", err)
-		} else {
-			log.Println("connected to database")
-		}
-		time.Sleep(5 * time.Second)
+	err = conn.Ping(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("DB connection is established")
 	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	http.ListenAndServe(os.Getenv("IP_ADDR")+":"+"8080", r)
 }
