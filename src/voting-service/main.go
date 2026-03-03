3a12a43237750db5ac14e54d8e98499e65397cc5
harik8 <khprasaad@gmail.com>
2026-03-03 14:46:46 +0100

    D-020326: Add voting service and cnpg

--- src/voting-service/main.go
+++ src/voting-service/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "context"
-    "log"
-    "time"
-    "github.com/jackc/pgx/v5"
-    "os"
+	"context"
+	"github.com/jackc/pgx/v5"
+	"log"
+	"os"
+	"time"
 )
 
 func main() {
-    conn, err := pgx.Connect(context.Background(), os.Getenv("CNPG_URI"))
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer conn.Close(context.Background())
+	conn, err := pgx.Connect(context.Background(), os.Getenv("CNPG_URI"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close(context.Background())
 
-    for {
-        if err = conn.Ping(context.Background()); err != nil {
-            log.Printf("ping failed: %v", err)
-        } else {
-            log.Println("connected to database")
-        }
-        time.Sleep(5 * time.Second)
-    }
+	for {
+		err = conn.Ping(context.Background())
+		if err != nil {
+			log.Printf("ping failed: %v", err)
+		} else {
+			log.Println("connected to database")
+		}
+		time.Sleep(5 * time.Second)
+	}
 }
