f239d7afbe08671eea532fc15167e106a0a427a6
harik8 <khprasaad@gmail.com>
2026-04-15 00:16:35 +0200

    D-140426: Add Chart.yaml

--- .bash/install-addons.sh
+++ .bash/install-addons.sh
@@ -10,28 +10,28 @@ helm repo update
 cd ../addons/charts
 
 echo "Installing alloy..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace alloy --version 1.3.1 --values alloy/values.yaml alloy grafana/alloy
+helm upgrade --install --rollback-on-failure --create-namespace --namespace alloy --values alloy/values.yaml alloy grafana/alloy
 
 echo "Installing argo-rollouts..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace argo-rollouts --version 2.39.6 --values argo-rollouts/values.yaml  argo-rollouts argo/argo-rollouts
+helm upgrade --install --rollback-on-failure --create-namespace --namespace argo-rollouts --values argo-rollouts/values.yaml  argo-rollouts argo/argo-rollouts
 
 echo "Installing grafana..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace grafana --version 10.1.2 --values grafana/values.yaml grafana grafana/grafana
+helm upgrade --install --rollback-on-failure --create-namespace --namespace grafana --values grafana/values.yaml grafana grafana/grafana
 
 echo "Installing loki..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace loki --version 6.43.0 --values loki/values.yaml loki grafana/loki
+helm upgrade --install --rollback-on-failure --create-namespace --namespace loki --values loki/values.yaml loki grafana/loki
 
 echo "Installing metrics-server..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace metrics-server --version 3.12.2 --values metrics-server/values.yaml metrics-server metrics-server/metrics-server
+helm upgrade --install --rollback-on-failure --create-namespace --namespace metrics-server --values metrics-server/values.yaml metrics-server metrics-server/metrics-server
 
 echo "Installing prometheus..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace prometheus --version 27.41.1 --values prometheus/values.yaml prometheus prometheus-community/prometheus
+helm upgrade --install --rollback-on-failure --create-namespace --namespace prometheus --values prometheus/values.yaml prometheus prometheus-community/prometheus
 
 echo "Installing cnpg..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace cnpg --version 0.27.1 --values cnpg/values.yaml cnpg cnpg/cloudnative-pg
+helm upgrade --install --rollback-on-failure --create-namespace --namespace cnpg --values cnpg/values.yaml cnpg cnpg/cloudnative-pg
 
-echo "Installing ingress-haproxy..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace ingress-haproxy --version 1.49.0 --values ingress-haproxy/values.yaml ingress-haproxy haproxy/kubernetes-ingress
+echo "Installing ingress-haproxy"
+helm upgrade --install --rollback-on-failure --create-namespace --namespace ingress-haproxy --values ingress-haproxy/values.yaml ingress-haproxy haproxy/kubernetes-ingress
 
 if [ $PAT ]; then
   cd ..
