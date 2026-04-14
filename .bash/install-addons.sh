abc43e9c1a575c35cdee702444c0d170e1ad8f92
harik8 <khprasaad@gmail.com>
2026-04-14 19:45:39 +0200

    D-140426: Replace ingress-nginx with ingress-haproxy

--- .bash/install-addons.sh
+++ .bash/install-addons.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
