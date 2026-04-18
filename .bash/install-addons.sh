9e0e4d8c4274fb847fa52570bcb83188b87dc541
harik8 <khprasaad@gmail.com>
2026-04-18 14:29:13 +0200

    D-140426: Add Chart.lock

--- .bash/install-addons.sh
+++ .bash/install-addons.sh
@@ -10,28 +10,35 @@ helm repo update
 cd ../addons/charts
 
 echo "Installing argo-rollouts..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace argo-rollouts --values argo-rollouts/values.yaml  argo-rollouts argo/argo-rollouts
+version=$(yq e '.dependencies[].version' argo-rollouts/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace argo-rollouts --version $version --values argo-rollouts/values.yaml  argo-rollouts argo/argo-rollouts
 
 echo "Installing grafana..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace grafana --values grafana/values.yaml grafana grafana/grafana
+version=$(yq e '.dependencies[].version' grafana/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace grafana --version $version --values grafana/values.yaml grafana grafana/grafana
 
 echo "Installing loki..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace loki --values loki/values.yaml loki grafana/loki
+version=$(yq e '.dependencies[].version' loki/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace loki --version $version --values loki/values.yaml loki grafana/loki
 
 echo "Installing metrics-server..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace metrics-server --values metrics-server/values.yaml metrics-server metrics-server/metrics-server
+version=$(yq e '.dependencies[].version' metrics-server/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace metrics-server --version $version --values metrics-server/values.yaml metrics-server metrics-server/metrics-server
 
 echo "Installing prometheus..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace prometheus --values prometheus/values.yaml prometheus prometheus-community/prometheus
+version=$(yq e '.dependencies[].version' prometheus/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace prometheus --version $version --values prometheus/values.yaml prometheus prometheus-community/prometheus
 
 echo "Installing cnpg..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace cnpg --values cnpg/values.yaml cnpg cnpg/cloudnative-pg
+version=$(yq e '.dependencies[].version' cnpg/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace cnpg --version $version --values cnpg/values.yaml cnpg cnpg/cloudnative-pg
 
 echo "Installing ingress-haproxy..."
-helm upgrade --install --rollback-on-failure --create-namespace --namespace ingress-haproxy --values ingress-haproxy/values.yaml ingress-haproxy haproxy/kubernetes-ingress
+version=$(yq e '.dependencies[].version' ingress-haproxy/Chart.yaml)
+helm upgrade --install --rollback-on-failure --create-namespace --namespace ingress-haproxy --version $version --values ingress-haproxy/values.yaml ingress-haproxy haproxy/kubernetes-ingress
 
 if [ $PAT ]; then
-  cd ../custom/gha-runner
+  cd ..
 
   echo "Installing gha-runner..."
   helm upgrade --install --rollback-on-failure \
@@ -39,6 +46,6 @@ if [ $PAT ]; then
     --namespace gha-runner \
     --set image.tag="$(grep 'ENV VERSION=' "custom/gha-runner/Dockerfile" | cut -d'=' -f2)" \
 	  --set env[1].value=$PAT \
-    -f .helm-tmpl/values.yaml -f custom/gha-runner/.helm/values.yaml -f custom/gha-runner/.helm/sandbox/values.yaml \
+    -f .helm-tmpl/values.yaml -f custom/gha-runner/.helm/values.yaml -f custom/gha-runner/.helm/wsl/values.yaml \
     gha-runner .helm-tmpl
 fi
